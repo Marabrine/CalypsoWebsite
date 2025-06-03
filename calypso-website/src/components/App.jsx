4769caac2d67ec6d9f7e64bf6ba7e88074f100d2
Marwan Maalouf <2354628@cmontmorency.qc.ca>
2025-06-03 13:05:18 -0400

    importation de la liste d'items

--- calypso-website/src/components/App.jsx
+++ calypso-website/src/components/App.jsx
@@ -9,16 +9,45 @@ function App() {
 
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
-    { path: "/search/:sTerm" },
-    {
-      path: "/items/guitars",
-      children: [
-        {
-          path: ":itemId",
-        },
-      ],
-    },
-    { path: "" },
+    // { path: "/search/:sTerm" },
+    // {
+    //   path: "/items",
+    //   children: [
+    //     {
+    //       path: "/guitar-bass",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/piano-keyboard",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/orchestral-strings",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/brass-wind",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/drums-percussion",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/special",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/accessories",
+    //       children: "/:itemId",
+    //     },
+    //     {
+    //       path: "/sonata-calypso",
+    //       children: "/:itemId",
+    //     },
+    //   ],
+    // },
+    // { path: "/contact" },
   ]);
   return (
     <>
