14c5ed33806a65770b635c78b4c897ad9ad4995e
Marwan Maalouf <2354628@cmontmorency.qc.ca>
2025-06-26 14:12:36 -0400

    more bug fixes for errorPage

--- calypso-website/src/containers/ErrorPage.jsx
+++ calypso-website/src/containers/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import Header from "../components/header/Header";
+import "./ErrorPage.css";
 
 export default function ErrorPage() {
   return (
     <div>
       <Header />
-      <h1>Page Introuvable!</h1>
+      <h1 className="error-msg">Page Introuvable!</h1>
     </div>
   );
 }
